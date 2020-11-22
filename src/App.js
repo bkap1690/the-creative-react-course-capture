a13e3096c611784f40a8adeaffd3b4f5080b846f
bkap31690 <48579856+bkap31690@users.noreply.github.com>
2020-11-22 14:38:40 -0800

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import GlobalStyle from './components/GlobalStyle';
+// Global Style
 // Import Pages
 import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
